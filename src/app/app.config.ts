7fd92903136e8599ec31da855da84ab2eeb75105
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2025-12-17 23:28:18 +0200

    Add Tauri process plugin and implement update check in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
 import {
   ApplicationConfig,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
 } from "@angular/core";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
 import Aura from '@primeuix/themes/aura';
+import { relaunch } from '@tauri-apps/plugin-process';
+import { check } from '@tauri-apps/plugin-updater';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from "./app.routes";
 
@@ -18,6 +21,13 @@ export const appConfig: ApplicationConfig = {
         preset: Aura
       },
       ripple: true
+    }),
+    provideAppInitializer(async () => {
+      const update = await check();
+      if (update) {
+        await update.downloadAndInstall();
+        await relaunch();
+      }
     })
   ],
 };
