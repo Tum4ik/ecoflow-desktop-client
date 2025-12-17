398d37fa00bea894e6a3bd7d84884721092b8452
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2025-12-18 00:23:46 +0200

    Refactor update logic in app configuration and improve error handling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import {
 } from "@angular/core";
 import { provideRouter, withComponentInputBinding } from "@angular/router";
 import Aura from '@primeuix/themes/aura';
-import { check } from '@tauri-apps/plugin-updater';
+import { relaunch } from '@tauri-apps/plugin-process';
+import { check, Update } from '@tauri-apps/plugin-updater';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from "./app.routes";
 
@@ -22,16 +23,22 @@ export const appConfig: ApplicationConfig = {
       ripple: true
     }),
     provideAppInitializer(async () => {
+      // todo: move update logic to update service
+      let update: Update | null = null;
       try {
-        const update = await check();
+        update = await check();
       } catch (error) {
-        console.log(error);
+        // can't check update - maybe first release
       }
 
-      // if (update) {
-      //   await update.downloadAndInstall();
-      //   await relaunch();
-      // }
+      if (update) {
+        try {
+          await update.downloadAndInstall();
+          await relaunch();
+        } catch (error) {
+          // can't download and install
+        }
+      }
     })
   ],
 };
